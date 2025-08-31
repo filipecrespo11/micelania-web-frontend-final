16f04c84cac880e1762eb6afc724673c1aee623c
filipecrespo11 <filipescrespo@gmail.com>
2025-08-31 09:26:15 -0300

    feat: Restore Authorization header from saved token on cold start

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx';
+import axios from 'axios'
+import App from './App.jsx'
+
+// Restore Authorization header from saved token on cold start / page reload
+const token = localStorage.getItem('token')
+if (token) {
+  axios.defaults.headers.common['Authorization'] = `Bearer ${token}`
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
